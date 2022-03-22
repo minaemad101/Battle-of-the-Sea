9d4b27e93ab3420d9fa58630b8921eb97e39de29
minaemad101 <mina.labib00@eng-st.cu.edu.eg>
2022-03-22 15:24:03 +0200

    first commit

---
+++ source/common/shader/shader.hpp
@@ -0,0 +1,68 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+#include <string>
+
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace our {
+
+    class ShaderProgram {
+
+    private:
+        //Shader Program Handle
+        GLuint program;
+
+    public:
+        void create();
+        void destroy();
+
+        ShaderProgram(){ program = 0; }
+        ~ShaderProgram(){ destroy(); }
+
+        bool attach(const std::string &filename, GLenum type) const;
+
+        bool link() const;
+
+        void use() { 
+            glUseProgram(this->program);
+            //TODO: call opengl to use the program identified by this->program
+        }
+
+        GLuint getUniformLocation(const std::string &name) {
+            //TODO: call opengl to get the uniform location for the uniform defined by name from this->program
+            return glGetUniformLocation(this->program,name.c_str());
+        }
+
+        void set(const std::string &uniform, GLfloat value) {
+            //TODO: call opengl to set the value to the uniform defined by name
+            glUniform1f(getUniformLocation(uniform),value);
+        }
+
+        void set(const std::string &uniform, glm::vec2 value) {
+            //TODO: call opengl to set the value to the uniform defined by name
+            glUniform2f(getUniformLocation(uniform),value.x,value.y);
+        }
+
+        void set(const std::string &uniform, glm::vec3 value) {
+            //TODO: call opengl to set the value to the uniform defined by name
+            glUniform3f(getUniformLocation(uniform),value.x,value.y,value.z);
+        }
+
+        void set(const std::string &uniform, glm::vec4 value) {
+            //TODO: call opengl to set the value to the uniform defined by name
+            glUniform4f(getUniformLocation(uniform),value.x,value.y,value.z,value.w);
+        }
+
+
+        //TODO: Delete the copy constructor and assignment operator
+        ShaderProgram (const ShaderProgram&) = delete;
+        ShaderProgram& operator= (const ShaderProgram&) = delete;
+        //Question: Why do we do this? Hint: Look at the deconstructor
+    };
+
+}
+
+#endif
