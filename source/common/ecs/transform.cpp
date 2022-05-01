f1ed74b219309870a385ea1a1af172836a497021
minaemad101 <mina.labib00@eng-st.cu.edu.eg>
2022-05-01 14:44:43 +0200

    requirment 2

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -10,7 +10,10 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 2) Write this function
-        return glm::mat4(1.0f);
+        glm::mat4 scalMat = glm::scale(glm::mat4(1.0f),scale);
+        glm::mat4 rotMat = glm::yawPitchRoll(rotation[1], rotation[0], rotation[2]);
+        glm::mat4 tranMat = glm::translate( glm::mat4(1.0f), position);        
+        return tranMat*rotMat*scalMat;
     }
 
      // Deserializes the entity data and components from a json object
